04fb219dec9ae44cf6e1a3c554175fa1c9cdf1e6
Jesus <167132617+CJiens@users.noreply.github.com>
2026-04-09 08:46:53 +0200

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 
 function Header() {
   return (
-    <header className="flex flex-col text-center h-20 m-[30px] bg-white shadow-sm border-b border-indigo-100">
+    <header className=" content-center  text-center h-20 m-[30px] bg-white shadow-sm border-b border-indigo-100">
       <h1 className="text-3xl font-bold bg-gradient-to-r from-indigo-600 to-blue-600 bg-clip-text text-transparent">Jesus Luis Perez Martines Grupo:302</h1>
     </header>
   );
